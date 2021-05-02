d86d83183afeff27979aa3f6370430297a57abd3
StarDesign-FS <79676166+StarDesign-FS@users.noreply.github.com>
2021-05-01 17:31:11 -0700

    Updated Application

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 600,
+    height: 300,
+    autoHideMenuBar: true,
+    webPreferences: {
+        nodeIntegration: true
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
